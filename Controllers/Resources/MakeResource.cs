0401ab9aa9e9d8e9f753ffdbafbb0ef4b3cf99ab
Sean <seon2you@hotmail.com>
2017-06-21 03:40:57 -0700

    added AutoMapper.Extensions.Microsoft.DependencyInjection --version 1.2.0

--- Controllers/Resources/MakeResource.cs
+++ Controllers/Resources/MakeResource.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,13 +10,12 @@ namespace Vega.Controllers.Resources
     public class MakeResource
     {
         public int Id { get; set; }
-        [Required]
-        [StringLength(255)]
+
         public string Name { get; set; }
-        public ICollection<Model> Models { get; set; }
-        public Make()
+        public ICollection<ModelResource> Models { get; set; }
+        public MakeResource()
         {
-            Models = new Collection<Model>();
+            Models = new Collection<ModelResource>();
         }
     }
 }
