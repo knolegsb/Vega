64bf2dc6c4a3808966cc98ea79d38cf77345f0ab
Sean <seon2you@hotmail.com>
2017-06-21 05:16:41 -0700

    add vehicle component

---
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -0,0 +1,8 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'vehicle-form',
+    templateUrl: './vehicle-form.component.html'
+})
+export class VehicleFormComponent {
+}
