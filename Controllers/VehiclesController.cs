f5816ed933229dd6e5b3ebc97d9c9df8dbcb0f1d
Sean <seon2you@hotmail.com>
2017-06-22 13:14:52 -0700

    mapping

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Vega.Core.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,6 +12,10 @@ namespace Vega.Controllers
     [Route("api/[controller]")]
     public class VehiclesController : Controller
     {
-        
+        [HttpPost("")]
+        public IActionResult CreateVehicle([FromBody]Vehicle vehicle)
+        {
+            return Ok(vehicle);
+        }
     }
 }
--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -12,10 +12,18 @@ namespace Vega.Mapping
     {
         public MappingProfile()
         {
+            // Domain to API Resource
             CreateMap<Make, MakeResource>();
             CreateMap<Model, ModelResource>();
             CreateMap<Model, KeyValuePairResource>();
             CreateMap<Feature, KeyValuePairResource>();
+
+            // API Resource to Domain
+            CreateMap<VehicleResource, Vehicle>()
+                .ForMember(v => v.ContactName, opt => opt.MapFrom(vr => vr.Contact.Name))
+                .ForMember(v => v.ContactEmail, opt => opt.MapFrom(vr => vr.Contact.Email))
+                .ForMember(v => v.ContactPhone, opt => opt.MapFrom(vr => vr.Contact.Phone))
+                .ForMember(v => v.Features, opt => opt.MapFrom(vr => vr.Features.Select(id => new VehicleFeature { FeatureId = id })));
         }
     }
 }
