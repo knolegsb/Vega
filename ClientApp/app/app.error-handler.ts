13c1b6e00c44bd017e8cc1184797d4af1f0fa749
Sean <seon2you@hotmail.com>
2017-06-29 12:58:31 -0700

    sentry raven

--- ClientApp/app/app.error-handler.ts
+++ ClientApp/app/app.error-handler.ts
@@ -1,18 +1,28 @@
-﻿import { ErrorHandler, Inject } from '@angular/core';
+﻿import { ErrorHandler, Inject, NgZone } from '@angular/core';
 import { ToastyService } from "ng2-toasty";
 
 export class AppErrorHandler implements ErrorHandler {
-    constructor( @Inject(ToastyService) private toastyService: ToastyService) { }
+    constructor(
+        private ngZone: NgZone,
+        @Inject(ToastyService) private toastyService: ToastyService) {
+    }
 
     handleError(error: any): void {
         //console.log("Error");
 
-        this.toastyService.error({
-            title: 'Error',
-            msg: 'An unexpected error happened.',
-            theme: 'bootstrap',
-            showClose: true,
-            timeout: 5000
-        });
+        //if (!isDevMode())
+        //    Raven.captureException(error.orginalError || error);
+        //else
+        //    throw error;
+
+        this.ngZone.run(() => {
+            this.toastyService.error({
+                title: 'Error',
+                msg: 'An unexpected error happened.',
+                theme: 'bootstrap',
+                showClose: true,
+                timeout: 5000
+            });
+        });        
     }
 }
--- webpack.config.vendor.js
+++ webpack.config.vendor.js
@@ -34,6 +34,7 @@ module.exports = (env) => {
                 'ng2-toasty/bundles/style-bootstrap.css',
                 'underscore',
                 'jquery',
+                'raven-js',
                 'zone.js',
             ]
         },
